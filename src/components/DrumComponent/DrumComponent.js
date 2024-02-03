0cc9acd165b59a43063e9a633ed90216c928a431
Enrique <76109775+enriiquee03@users.noreply.github.com>
2024-02-04 00:19:07 +0100

    Update component name and class names to 'drum-machine' and 'drum-pad'.

--- src/components/DrumComponent/DrumComponent.js
+++ src/components/DrumComponent/DrumComponent.js
@@ -90,7 +90,7 @@ const DrumComponent = () => {
 
 
   return (
-    <div className="w-1/2 mx-auto bg-blue bg-white p-8 rounded shadow-md text-center">
+    <div className="drum-machine w-1/2 mx-auto bg-blue bg-white p-8 rounded shadow-md text-center">
 
       <h1 className="text-2xl font-bold mb-4">Control de Audio</h1>
 
@@ -124,15 +124,15 @@ const DrumComponent = () => {
 
       {/* Nueve Botones con Letras */}
       <div className="grid grid-cols-3 gap-4">
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("Q")}>Q</button>
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("W")}>W</button>
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("E")}>E</button>
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("A")}>A</button>
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("S")}>S</button>
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("D")}>D</button>
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("Z")}>Z</button>
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("X")}>X</button>
-        <button className="bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("C")}>C</button>
+        <button className="drum-pad bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("Q")}>Q</button>
+        <button className="drum-pad bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("W")}>W</button>
+        <button className="drum-pad bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("E")}>E</button>
+        <button className="drum-pad bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("A")}>A</button>
+        <button className="drum-pad bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("S")}>S</button>
+        <button className="drum-pad bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("D")}>D</button>
+        <button className="drum-pad bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("Z")}>Z</button>
+        <button className="drum-pad bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("X")}>X</button>
+        <button className="drum-pad bg-yellow-500 text-white px-4 py-2 rounded-full" onClick={() => reproducir("C")}>C</button>
       </div>
 
     </div>
