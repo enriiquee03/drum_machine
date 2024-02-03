b10f7214c7dde7eeb177afa3007784f9dbc84835
Enrique <76109775+enriiquee03@users.noreply.github.com>
2024-02-04 00:10:33 +0100

    Update volume control and toggle switch for drum machine.

--- src/components/DrumComponent/DrumComponent.js
+++ src/components/DrumComponent/DrumComponent.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 const DrumComponent = () => {
   const [encendido, setEncendido] = useState(true);
   const [tipoBanco, setBanco] = useState(true);
-  const [volumen, setVolumen] = useState(100)
+
+
+
 
   //Preparamos los audios que vamos a usar
-
   const audios = [
     {
       key : "Q",
@@ -54,32 +55,27 @@ const DrumComponent = () => {
     }
   ]
 
-
-
-  var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'); 
-  const changeVolume = (number) => {
-    console.log(audio.volume)
-    audio.volume = 0.3
-    setVolumen(parseFloat(number / 100));
-    console.log(audio.volume)
-    console.log(volumen)
-  };
-
   const toggleEncendido = () => {
     setEncendido(!encendido);
   };
 
   const toggleBanco = () => {
     setBanco(!tipoBanco);
+    console.log(tipoBanco)
   };
 
+
   const reproducir = (key) => {
-    
     audios.forEach(a => {
-      if(a.key.toLowerCase() === key.toLowerCase()){
-        console.log("Si")
-        console.log(a.audioB1)
-        audio = a.audioB1;
+      if(a.key.toLowerCase() === key.toLowerCase() && encendido && tipoBanco){
+        let audioVolume = document.getElementById("volume").value
+        let audio = a.audioB1;
+        audio.volume = (audioVolume/100)
+        audio.play()
+      }else if(a.key.toLowerCase() === key.toLowerCase() && encendido && !tipoBanco){
+        let audioVolume = document.getElementById("volume").value
+        let audio = a.audioB2;
+        audio.volume = (audioVolume/100)
         audio.play();
       }
     });
@@ -88,7 +84,7 @@ const DrumComponent = () => {
   };
 
   document.addEventListener("keypress", (e) => {
-    alert("La tecla que has pulsado es: " + e.key)
+    reproducir(e.key)
   });
 
 
@@ -109,19 +105,22 @@ const DrumComponent = () => {
       {/* Barra de Volumen */}
       <div className="mb-4">
         <label htmlFor="volume" className="block text-sm font-medium text-gray-600">Volumen</label>
-        <input type="range"  id="volume" name="volume" min="0" max="100" aria-valuenow={50}   onChange={() => {changeVolume(document.getElementById("volume").value)}} className="w-full" />
+        <input type="range"  id="volume" name="volume" min="0" max="100" aria-valuenow={50}   className="w-full" />
       </div>
 
       {/* Botón para Cambiar Banco */}
-      <label for="Toggle1" className="inline-flex items-center space-x-4 cursor-pointer dark:text-black-100 mb-10">
-        <span>Banco 1</span>
-        <span className="relative">
-          <input id="Toggle1" aria-checked= 'true' type="checkbox" className="hidden peer" onClick={() => toggleBanco} />
-          <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-violet-400"></div>
-          <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800"></div>
-        </span>
-        <span>Banco 2</span>
-      </label>
+      <div className='flex justify-center mb-4'>
+        <p className='mr-3'>Banco 1</p> 
+        <input
+          className=" bg-green-500 ml-[0.15rem] mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-blue-500  checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+          type="checkbox"
+          role="switch"
+          id="flexSwitchCheckDefault"
+          onChange={toggleBanco}
+        />
+          <p className='ml-3'>Banco 2</p> 
+      </div>
+
 
       {/* Nueve Botones con Letras */}
       <div className="grid grid-cols-3 gap-4">
